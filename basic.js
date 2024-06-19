5129992f342b4b05ca8d32c0b0181f0c37db7693
KANISHKAR <kaniskarthangaraj07@gmail.com>
2024-06-19 18:08:06 +0530

    today activities

---
+++ basic.js
@@ -0,0 +1,56 @@
+//let a=9;
+/*
+let b=10;
+document.write(b);
+
+
+let car={
+    carname:"honda",
+    carcolor:"black",
+    carno:527
+}
+for(let a in car){
+    console.log(car[a]);
+}
+let d=10;
+let v=20;
+let c=v+d;
+document.write(c);*/
+
+/*if(a>18){
+    document.write("eligible");
+}
+else{
+    document.write("not eligible");
+}*/
+/*
+let s="kanishkarraina"
+document.write("<br>",s.length);
+document.write("<br>",s.slice(0,6));
+document.write("<br>",s.substring(0,7));
+document.write("<br>",s.substr(0,2));
+document.write("<br>",s.toUpperCase());
+*/
+// var a=new Array();
+// a[0]=21;
+// console.log(a[0]);
+// var str=[0,3,4,5,6,3,2,1,7,4,8];
+
+// document.write("<br>",str.sort());
+function run(a){
+    var a=[2,3,4,5,6];
+    console.log(b);
+}
+let add=(a)=>{
+    let c=20
+    console.log(a);
+}
+let b=10
+let a=30
+add(a);
+
+
+
+
+
+
